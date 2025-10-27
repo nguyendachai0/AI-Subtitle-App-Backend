71e4cb14a457fba82e554d0c8aa702c155419324
Hai Nguyen <nguyendmim@gmail.com>
2025-10-27 14:06:12 +0700

    Add origin for domain from vercel

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: "http://localhost:3000",
+		origin: ["http://localhost:3000", "https://*.vercel.app"],
 		credentials: true,
 	});
 	await app.listen(3001);
